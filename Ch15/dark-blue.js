00ddbd774e345a4665e687716cf09695bd81b073
Brendan Duke <dukebw@mcmaster.ca>
2015-01-09 09:21:18 -0500

    Adding collision detection

--- Ch15/dark-blue.js
+++ Ch15/dark-blue.js
@@ -190,5 +190,44 @@ Level.prototype.isFinished = function() {
   return this.status !== null && this.finishDelay < 0;
 };
 
+// NOTE(brendan): tells us whether a rectangle overlaps with any non-empty
+// space on the background grid.
+Level.prototype.obstacleAt = function(pos, size) {
+  var xStart = Math.floor(pos.x);
+  var xEnd = Math.floor(pos.x + size.x);
+  var yStart = Math.floor(pos.y);
+  var yEnd = Math.floor(pos.y + size.y);
+
+  if(xStart < 0 || xEnd > this.width || yStart < 0) {
+    return "wall";
+  }
+  if(yEnd > this.height) {
+    return "lava";
+  }
+  for(var y = yStart; y < yEnd; ++y) {
+    for(var x = xStart; x < xEnd; ++x) {
+      var fieldType = this.grid[y][x];
+      if(fieldType) {
+        return fieldType;
+      }
+    }
+  }
+};
+
+// NOTE(brendan): Scans array of actors, looking for an actor that overlaps
+// the actor given as an argument.
+Level.prototype.actorAt = function(actor) {
+  for(var i = 0; i < this.actors.length; ++i) {
+    var other = this.actors[i];
+    if(other !== actor &&
+       actor.pos.x + actor.size.x > other.pos.x &&
+       actor.pos.x < other.pos.x + other.size.x &&
+       actor.pos.y + actor.size.y > other.pos.y &&
+       actor.pos.y < other.pos.y + other.size.y) {
+      return other;
+    }
+  }
+};
+
 var simpleLevel = new Level(simpleLevelPlan);
 var display = new DOMDisplay(document.body, simpleLevel);
